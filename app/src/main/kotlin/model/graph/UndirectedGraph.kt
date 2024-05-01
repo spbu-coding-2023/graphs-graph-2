33a5ea8d3ea7c6068e211cc72d7676a860be38fa
qruty <64466788+qrutyy@users.noreply.github.com>
2024-05-01 18:00:16 +0300

    fix: remove EdgeType refinement. small corrections

--- app/src/main/kotlin/model/graph/UndirectedGraph.kt
+++ app/src/main/kotlin/model/graph/UndirectedGraph.kt
@@ -1,13 +1,13 @@
 package model.graph
 
-class UndirectedEdge<V>(override val vertex1: Vertex< V>?, override val vertex2: Vertex< V>?) : Edge< V>
+open class UndirectedEdge<D>(override val vertex1: Vertex< D>, override val vertex2: Vertex< D>) : Edge< D>
 
-class UndirectedGraph<V>: Graph<V, UndirectedEdge<V>>() {
-    val adjacencyMap: MutableMap<Vertex< V>, ArrayList<UndirectedEdge<V>>> = mutableMapOf()
-    private val edges: MutableSet<Edge< V>> = mutableSetOf()
-    private var currentId: Int = 0
+class UndirectedGraph<D>: Graph<D> {
+    private val adjacencyMap: MutableMap<Vertex< D>, ArrayList<UndirectedEdge<D>>> = mutableMapOf()
+    private val edges: MutableSet<Edge< D>> = mutableSetOf()
+    override var currentId: ULong = 0u
 
-    override fun addEdge(node1: V, node2: V) {
+    override fun addEdge(node1: D, node2: D) {
         val vertex1 = adjacencyMap.keys.find { it.data == node1 } ?: run {
             addVertex(node1)
             adjacencyMap.keys.find { it.data == node1 }
@@ -16,24 +16,23 @@ class UndirectedGraph<V>: Graph<V, UndirectedEdge<V>>() {
             addVertex(node2)
             adjacencyMap.keys.find { it.data == node2 }
         }
-
-        val edge = UndirectedEdge(vertex1, vertex2)
-
-        adjacencyMap[vertex1]?.add(edge)
-        adjacencyMap[vertex2]?.add(edge)
+        if (vertex1 != null && vertex2 != null) {
+            val edge = UndirectedEdge(vertex1, vertex2)
+            adjacencyMap[vertex1]?.add(edge)
+            adjacencyMap[vertex2]?.add(edge)
+        }
     }
 
-    override fun addVertex(value: V) {
-        val vertex = Vertex(currentId, value)
-        currentId++
+    override fun addVertex(value: D) {
+        val vertex = Vertex(currentId++, value)
         adjacencyMap[vertex] = ArrayList()
     }
 
-    override fun getEdges(): List<UndirectedEdge<V>> {
+    override fun getEdges(): List<UndirectedEdge<D>> {
         return adjacencyMap.values.flatten()
     }
 
-    override fun getVertices(): List<Vertex<V>> {
+    override fun getVertices(): List<Vertex<D>> {
         return adjacencyMap.keys.toList()
     }
 }
