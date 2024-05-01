51613f39a79904ad85a7fd410714f870858f1bbe
Karim Shakirov <karimshak06@gmail.com>
2024-05-01 20:57:41 +0300

    feat(Graph): change to abstract class, add properties, deletion methods and method implementations
    Change graph to abstract class from interface. Add adjacencyMap and edges properties. Add deleteVertex and deleteEdge methods. Add implementation for addVertex, getVertices, getEdges methods.

--- app/src/main/kotlin/model/graph/Graph.kt
+++ app/src/main/kotlin/model/graph/Graph.kt
@@ -1,9 +1,39 @@
 package model.graph
 
-interface Graph<D>{
-    val currentId: ULong
-    fun addVertex(value: D)
-    fun addEdge(node1: D, node2: D)
-    fun getVertices(): List<Vertex<D>>
-    fun getEdges(): List<Edge<D>>
+abstract class Graph<D> {
+    val adjacencyMap: MutableMap<Vertex<D>, ArrayList<Vertex<D>>> = mutableMapOf()
+    val edges: MutableSet<Edge<D>> = mutableSetOf()
+    var currentId = 0uL
+    // TODO("add visibility modifiers for properties")
+    fun addVertex(data: D) {
+        val newVertex = Vertex(currentId++, data)
+        adjacencyMap[newVertex] = ArrayList()
+    }
+
+    fun deleteVertex(vertexToDelete: Vertex<D>): D? {
+        val adjacentVertices = adjacencyMap[vertexToDelete] ?: return null
+        for (adjacentVertex in adjacentVertices) adjacencyMap[adjacentVertex]?.remove(vertexToDelete)
+        adjacencyMap.remove(vertexToDelete)
+
+        return vertexToDelete.data
+    }
+
+    abstract fun addEdge(vertex1: Vertex<D>, vertex2: Vertex<D>)
+
+    fun deleteEdge(edgeToDelete: Edge<D>): Edge<D>? {
+        if (edgeToDelete !in edges) return null
+
+        val vertex1 = edgeToDelete.vertex1
+        val vertex2 = edgeToDelete.vertex2
+
+        adjacencyMap[vertex1]?.remove(vertex2)
+        adjacencyMap[vertex2]?.remove(vertex1)
+        edges.remove(edgeToDelete)
+
+        return edgeToDelete
+    }
+
+    fun getVertices() = adjacencyMap.keys.toList()
+
+    fun getEdges() = edges.toList()
 }
