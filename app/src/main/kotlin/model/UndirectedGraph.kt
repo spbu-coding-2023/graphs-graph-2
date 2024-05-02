68471fccbb47a555ccc0aa9411b71fa30801dd19
Karim Shakirov <karimshak06@gmail.com>
2024-05-03 00:36:38 +0300

    style: apply ktfmt formatting

--- app/src/main/kotlin/model/UndirectedGraph.kt
+++ app/src/main/kotlin/model/UndirectedGraph.kt
@@ -7,8 +7,7 @@ import model.edges.UndirectedEdge
 
 open class UndirectedGraph<D> : Graph<D>() {
     override fun addEdge(vertex1: Vertex<D>, vertex2: Vertex<D>): UndirectedEdge<D> {
-        if (vertex1 == vertex2)
-            throw IllegalArgumentException("Vertices are the same")
+        if (vertex1 == vertex2) throw IllegalArgumentException("Vertices are the same")
         if (vertex1 !in adjacencyMap.keys || vertex2 !in adjacencyMap.keys)
             throw IllegalArgumentException("Vertex1 or vertex2 are not in the graph")
 
