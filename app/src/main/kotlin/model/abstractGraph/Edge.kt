bfa2aa2a7f97838de7a430297957cc9cc046f51c
Karim Shakirov <karimshak06@gmail.com>
2024-05-04 12:37:12 +0300

    refactor: change Edge from interface to abstract class

--- app/src/main/kotlin/model/abstractGraph/Edge.kt
+++ app/src/main/kotlin/model/abstractGraph/Edge.kt
@@ -1,6 +1,6 @@
 package model.abstractGraph
 
-interface Edge<D> {
-    val vertex1: Vertex<D>
-    val vertex2: Vertex<D>
+abstract class Edge<D> {
+    abstract val vertex1: Vertex<D>
+    abstract val vertex2: Vertex<D>
 }
