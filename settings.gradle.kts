e5f3b8df1a7843f38dd14eb77cbfd0a57d6cfa05
Vlasenco Daniel <vlasenko.daniil26@gmail.com>
2024-05-09 17:19:35 +0300

    chore: change kotlin version to 1.9.22

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,7 +17,7 @@ dependencyResolutionManagement {
     }
     versionCatalogs {
         create("libs") {
-            version("kotlin", "1.9.20")
+            version("kotlin", "1.9.22")
             plugin("kotlin-jvm", "org.jetbrains.kotlin.jvm").versionRef("kotlin")
             plugin("compose", "org.jetbrains.compose").version("1.6.2")
             library("koin-core", "io.insert-koin:koin-core:3.5.3")
