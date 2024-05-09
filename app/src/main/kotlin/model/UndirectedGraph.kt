1fe5179a6beda4813a47d782db903c40cdd5dc26
Vlasenco Daniel <vlasenko.daniil26@gmail.com>
2024-05-09 13:58:41 +0300

    feat: implement Tarjan's bridge finding algorithm

--- app/src/main/kotlin/model/UndirectedGraph.kt
+++ app/src/main/kotlin/model/UndirectedGraph.kt
@@ -34,5 +34,60 @@ open class UndirectedGraph<D> : Graph<D>() {
 
         return edgeToRemove
     }
+
+
+
+    fun findBridges(): List<Edge<D>> {
+        val bridges = mutableListOf<Edge<D>>()
+
+        val vertices = getVertices()
+
+        val graphSize = vertices.size
+
+        val discTime = MutableList(graphSize) { -1 }
+        val minDiscTime = MutableList(graphSize) { -1 }
+
+        val visited = MutableList(graphSize) { false }
+        val parent = MutableList(graphSize) { -1 }
+
+        var iterationCount = 0
+
+        fun doDFSToFindBrigesFromVertex(vertex: Vertex<D>) {
+            visited[vertex.id] = true
+
+            iterationCount++
+            discTime[vertex.id] = iterationCount
+            minDiscTime[vertex.id] = iterationCount
+
+            for (neighbour in getNeighbours(vertex)) {
+                if (!visited[neighbour.id]) {
+                    parent[neighbour.id] = vertex.id
+
+                    doDFSToFindBrigesFromVertex(neighbour)
+
+                    minDiscTime[vertex.id] =
+                        min(minDiscTime[vertex.id], minDiscTime[neighbour.id])
+
+                    if (minDiscTime[neighbour.id] > discTime[vertex.id]) {
+                        val bridgeFound = edges.find { it.isIncident(vertex) && it.isIncident(neighbour) }
+                            ?: throw NoSuchElementException("Can't find edge between vertices with ids ${vertex.id} and ${neighbour.id}")
+
+                        bridges.add(bridgeFound)
+                    }
+                } else if (neighbour.id != parent[vertex.id]) {
+                    minDiscTime[vertex.id] =
+                        min(minDiscTime[vertex.id], discTime[neighbour.id])
+                }
+            }
+        }
+
+        for (v in vertices) {
+            if (visited[v.id]) continue
+
+            doDFSToFindBrigesFromVertex(v)
+        }
+
+        return bridges
+    }
 }
 
