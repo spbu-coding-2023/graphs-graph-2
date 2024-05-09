7af4dad3959e9a2f05b64f1df249f96c6004e65e
Vlasenco Daniel <vlasenko.daniil26@gmail.com>
2024-05-09 16:34:48 +0300

    chore: remove all ktfmt-related tasks, reformat gradle files

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,11 +19,8 @@ dependencyResolutionManagement {
         create("libs") {
             version("kotlin", "1.9.20")
             plugin("kotlin-jvm", "org.jetbrains.kotlin.jvm").versionRef("kotlin")
-
             plugin("compose", "org.jetbrains.compose").version("1.6.2")
-
             library("koin-core", "io.insert-koin:koin-core:3.5.3")
-
             library("junit-jupiter", "org.junit.jupiter:junit-jupiter:5.10.2")
         }
     }
