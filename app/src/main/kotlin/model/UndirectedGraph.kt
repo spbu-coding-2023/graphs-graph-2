b17d3501352317708d516484e8c757410fab29a3
Daniel Vlasenco <vlasenko.daniil26@gmail.com>
2024-05-12 18:13:35 +0300

    feat: implement Tarjan's bridge finding algorithm #15
    * chore: resolve merge conflict
    
    * feat: implement Tarjan's bridge finding algorithm
    
    * refactor: bridge finding algorithm
    
    * refactor: min spanning tree finding algorithm

--- app/src/main/kotlin/model/UndirectedGraph.kt
+++ app/src/main/kotlin/model/UndirectedGraph.kt
@@ -1,5 +1,6 @@
 package model
 
+import kotlin.math.min
 import model.abstractGraph.Edge
 import model.abstractGraph.Graph
 import model.abstractGraph.Vertex
@@ -33,5 +34,60 @@ open class UndirectedGraph<D> : Graph<D>() {
 
         return edgeToRemove
     }
-}
 
+    fun findBridges(): List<Edge<D>> {
+        val bridges = mutableListOf<Edge<D>>()
+
+        val vertices = getVertices()
+        val graphSize = vertices.size
+
+        val discoveryTime = MutableList(graphSize) { -1 }
+        val minDiscoveryTime = MutableList(graphSize) { -1 }
+
+        val visitedList = MutableList(graphSize) { false }
+        val parentList = MutableList(graphSize) { -1 }
+
+        var iterationCount = 0
+
+        fun doDFSToFindBridgesFromVertex(vertex: Vertex<D>) {
+            visitedList[vertex.id] = true
+
+            iterationCount++
+            discoveryTime[vertex.id] = iterationCount
+            minDiscoveryTime[vertex.id] = iterationCount
+
+            for (neighbour in getNeighbours(vertex)) {
+                if (neighbour.id == parentList[vertex.id]) continue
+
+                if (visitedList[neighbour.id]) {
+                    minDiscoveryTime[vertex.id] =
+                        min(minDiscoveryTime[vertex.id], discoveryTime[neighbour.id])
+
+                    continue
+                }
+
+                parentList[neighbour.id] = vertex.id
+
+                doDFSToFindBridgesFromVertex(neighbour)
+
+                minDiscoveryTime[vertex.id] =
+                    min(minDiscoveryTime[vertex.id], minDiscoveryTime[neighbour.id])
+
+                if (minDiscoveryTime[neighbour.id] > discoveryTime[vertex.id]) {
+                    val bridgeFound = edges.find { it.isIncident(vertex) && it.isIncident(neighbour) }
+                        ?: throw NoSuchElementException("Can't find edge between vertices with ids ${vertex.id} and ${neighbour.id}")
+
+                    bridges.add(bridgeFound)
+                }
+            }
+        }
+
+        for (v in vertices) {
+            if (visitedList[v.id]) continue
+
+            doDFSToFindBridgesFromVertex(v)
+        }
+
+        return bridges
+    }
+}
