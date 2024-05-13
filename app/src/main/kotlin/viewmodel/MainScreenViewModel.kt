b3152e2d72fb1d500e6f0536f551c2c1e665beee
qruty <64466788+qrutyy@users.noreply.github.com>
2024-05-13 23:47:46 +0300

    feat: implement summarizing main screen VM

--- app/src/main/kotlin/viewmodel/MainScreenViewModel.kt
+++ app/src/main/kotlin/viewmodel/MainScreenViewModel.kt
@@ -1,5 +1,21 @@
 package viewmodel
 
+import androidx.compose.runtime.mutableStateOf
 import model.abstractGraph.Graph
+import viewmodel.graph.GraphViewModel
+import viewmodel.graph.TestRepresentation
 
-class MainScreenViewModel<D>(graph: Graph<D>) {}
+class MainScreenViewModel<D>(graph: Graph<D>) {
+    val showVerticesData = mutableStateOf(false)
+    val showVerticesIds = mutableStateOf(false)
+    val graphViewModel = GraphViewModel(graph, showVerticesIds, showVerticesData)
+
+    init { // here will be a placement-function call
+        TestRepresentation().place(740.0, 650.0, graphViewModel.verticesVM)
+    }
+
+//    fun setEdgeColor
+//
+//    fun setVerticesColor()
+
+}
