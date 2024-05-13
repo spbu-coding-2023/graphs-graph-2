d16773823d2618500d22560f073d96705b8be620
Karim Shakirov <karimshak06@gmail.com>
2024-05-13 14:03:52 +0300

    test: add junit dependency to gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,10 +6,17 @@ plugins {
 dependencies {
     implementation(compose.desktop.currentOs)
     implementation(libs.koin.core)
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 }
 
 compose.desktop {
     application {
         mainClass = "MainKt"
     }
+}
+
+tasks.test {
+    useJUnitPlatform()
 }
