f5f8ab21a93423ff1d68b5d79d7fbf929d611ef3
Karim Shakirov <karimshak06@gmail.com>
2024-05-14 00:21:41 +0300

    test: add test classes and inner test classes for methods, parameterized test annotation #17
    * test: add junit dependency to gradle
    
    * test: add test classes and package structure, inner test classes for methods
    
    * test: add custom annotation for parameterized tests in abstract graph test class

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,10 +6,18 @@ plugins {
 dependencies {
     implementation(compose.desktop.currentOs)
     implementation(libs.koin.core)
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 }
 
 compose.desktop {
     application {
         mainClass = "MainKt"
     }
+}
+
+tasks.test {
+    useJUnitPlatform()
 }
---
+++ app/src/test/kotlin/model/WeightedUndirectedGraphTest.kt
@@ -0,0 +1,17 @@
+package model
+
+import org.junit.jupiter.api.Nested
+
+class WeightedUndirectedGraphTest {
+    @Nested
+    inner class AddEdgeTest {}
+
+    @Nested
+    inner class RemoveEdgeTest {}
+
+    @Nested
+    inner class FindShortestPathDijkstraTest {}
+
+    @Nested
+    inner class FindMinSpanningTreeTest {}
+}
