1f87ec834035007f0730b9968ad47ed836bc9c3a
Karim Shakirov <karimshak06@gmail.com>
2024-05-15 18:47:51 +0300

    feat: add equals method override in vertex and edge for more convenient comparison

--- app/src/main/kotlin/model/abstractGraph/Edge.kt
+++ app/src/main/kotlin/model/abstractGraph/Edge.kt
@@ -1,6 +1,7 @@
 package model.abstractGraph
 
 class Edge<D>(val vertex1: Vertex<D>, val vertex2: Vertex<D>) {
-
     fun isIncident(vertex: Vertex<D>) = (vertex1 == vertex || vertex2 == vertex)
+
+    override fun equals(other: Any?) = (other is Edge<*>) && (vertex1 == other.vertex1) && (vertex2 == other.vertex2)
 }
--- app/src/main/kotlin/model/abstractGraph/Vertex.kt
+++ app/src/main/kotlin/model/abstractGraph/Vertex.kt
@@ -1,3 +1,5 @@
 package model.abstractGraph
 
-class Vertex<D>(val id: Int, val data: D)
+class Vertex<D>(val id: Int, val data: D) {
+    override fun equals(other: Any?) = (other is Vertex<*>) && (id == other.id) && (data == other.data)
+}
