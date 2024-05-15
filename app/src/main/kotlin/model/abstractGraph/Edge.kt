c23350f7d7598b1e21b966f984192f384aa02ad5
Karim Shakirov <karimshak06@gmail.com>
2024-05-16 01:20:09 +0300

    fix: remove equals override method in vertex and edge

--- app/src/main/kotlin/model/abstractGraph/Edge.kt
+++ app/src/main/kotlin/model/abstractGraph/Edge.kt
@@ -2,6 +2,4 @@ package model.abstractGraph
 
 class Edge<D>(val vertex1: Vertex<D>, val vertex2: Vertex<D>) {
     fun isIncident(vertex: Vertex<D>) = (vertex1 == vertex || vertex2 == vertex)
-
-    override fun equals(other: Any?) = (other is Edge<*>) && (vertex1 == other.vertex1) && (vertex2 == other.vertex2)
 }
--- app/src/main/kotlin/model/abstractGraph/Vertex.kt
+++ app/src/main/kotlin/model/abstractGraph/Vertex.kt
@@ -1,5 +1,3 @@
 package model.abstractGraph
 
-class Vertex<D>(val id: Int, val data: D) {
-    override fun equals(other: Any?) = (other is Vertex<*>) && (id == other.id) && (data == other.data)
-}
+class Vertex<D>(val id: Int, val data: D)
