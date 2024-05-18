b8fb2aea3124af0ea03995e7755507a7ea170bf9
Karim Shakirov <karimshak06@gmail.com>
2024-05-18 14:59:22 +0300

    test: add custom annotations for testing both weighted and unweighted graphs

--- app/src/test/kotlin/model/UndirectedGraphTest.kt
+++ app/src/test/kotlin/model/UndirectedGraphTest.kt
@@ -4,7 +4,26 @@ import model.abstractGraph.Edge
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Nested
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
+
+
+@ParameterizedTest(name = "{0}")
+@ArgumentsSource(WeightedAndUnweightedUndirectedGraphsProvider::class)
+@Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.RUNTIME)
+private annotation class TestBothUndirectedGraphs
+
+class WeightedAndUnweightedUndirectedGraphsProvider : ArgumentsProvider {
+    override fun provideArguments(context: ExtensionContext?): Stream<Arguments> = Stream.of(
+        Arguments.of(UndirectedGraph<Int>()),
+        Arguments.of(WeightedUndirectedGraph<Int>())
+    )
+}
 
 class UndirectedGraphTest {
     private lateinit var graph: UndirectedGraph<Int>
