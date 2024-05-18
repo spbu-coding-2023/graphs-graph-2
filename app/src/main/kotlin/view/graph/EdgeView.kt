984e639119e159d0bb39625df1676602d4dd19a2
Vlasenco Daniel <vlasenko.daniil26@gmail.com>
2024-05-18 12:33:46 +0300

    feat: move all logic from edge view to edge view model

--- app/src/main/kotlin/view/graph/EdgeView.kt
+++ app/src/main/kotlin/view/graph/EdgeView.kt
@@ -8,53 +8,23 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.drawscope.Fill
-import androidx.compose.ui.unit.Dp
 import viewmodel.WindowViewModel
 import viewmodel.graph.EdgeViewModel
-import kotlin.math.sqrt
-
-const val ARROW_SIZE = 20f
-const val ARROW_DEPTH = 2.5f
-const val SQRT_3 = 1.732f
 
 @Composable
 fun <D> EdgeView(viewModel: EdgeViewModel<D>) {
-
     val windowVM = WindowViewModel()
     windowVM.SetCurrentDimensions()
 
-    val radius = viewModel.firstVertex.radius
+    val firstVertexCenter = viewModel.calculateFirstVertexCenter()
+    val secondVertexCenter = viewModel.calculateSecondVertexCenter()
 
-    val firstVertexCenterX = viewModel.firstVertex.x.value + radius
-    val firstVertexCenterY = viewModel.firstVertex.y.value + radius
+    val firstVertexCenterX = firstVertexCenter.first
+    val firstVertexCenterY = firstVertexCenter.second
+    val secondVertexCenterX = secondVertexCenter.first
+    val secondVertexCenterY = secondVertexCenter.second
 
-    val secondVertexCenterX = viewModel.secondVertex.x.value + radius
-    val secondVertexCenterY = viewModel.secondVertex.y.value + radius
-
-    // find the coordinates of vector representing edge
-    val vectorX = secondVertexCenterX - firstVertexCenterX
-    val vectorY = secondVertexCenterY - firstVertexCenterY
-
-    val len = sqrt(vectorX * vectorX + vectorY * vectorY)
-
-    val normedVectorX = vectorX / len
-    val normedVectorY = vectorY / len
-
-    // rotate normed vector by Pi/6
-    val aX = normedVectorX * SQRT_3/2 - normedVectorY * 1/2
-    val aY = normedVectorX * 1/2 + normedVectorY * SQRT_3/2
-
-    // rotate normed vector by negative Pi/6
-    val bX = normedVectorX * SQRT_3/2 + normedVectorY * 1/2
-    val bY = -normedVectorX * 1/2 + normedVectorY * SQRT_3/2
-
-    val arrowEndPointX = secondVertexCenterX - normedVectorX * (radius.value - ARROW_DEPTH)
-    val arrowEndPointY = secondVertexCenterY - normedVectorY * (radius.value - ARROW_DEPTH)
-
-    val leftArrowPointX = arrowEndPointX - aX * ARROW_SIZE
-    val leftArrowPointY = arrowEndPointY - aY * ARROW_SIZE
-    val rightArrowPointX = arrowEndPointX - bX * ARROW_SIZE
-    val rightArrowPointY = arrowEndPointY - bY * ARROW_SIZE
+    val arrowPoints = viewModel.calculateArrowPoints()
 
     Canvas(modifier = Modifier.fillMaxSize()) {
         drawLine(
@@ -72,21 +42,21 @@ fun <D> EdgeView(viewModel: EdgeViewModel<D>) {
             ),
         )
 
-        if (viewModel.isDirected.value) {
+        if (arrowPoints.isNotEmpty()) {
             val trianglePath = Path()
 
             // these points represent the vertices of a triangle
             trianglePath.moveTo(
-                arrowEndPointX.toPx(),
-                arrowEndPointY.toPx(),
+                arrowPoints[0].first.toPx(),
+                arrowPoints[0].second.toPx(),
             )
             trianglePath.lineTo(
-                leftArrowPointX.toPx(),
-                leftArrowPointY.toPx()
+                arrowPoints[1].first.toPx(),
+                arrowPoints[1].second.toPx()
             )
             trianglePath.lineTo(
-                rightArrowPointX.toPx(),
-                rightArrowPointY.toPx()
+                arrowPoints[2].first.toPx(),
+                arrowPoints[2].second.toPx()
             )
             trianglePath.close()
 
@@ -97,8 +67,4 @@ fun <D> EdgeView(viewModel: EdgeViewModel<D>) {
             )
         }
     }
-}
-
-private operator fun Dp.times(other: Dp): Float {
-    return this.value * other.value
 }
