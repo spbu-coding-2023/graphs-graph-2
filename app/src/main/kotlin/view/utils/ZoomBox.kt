68d8afd3066fec28c1c64a5584f25c0e0331d07e
qruty <64466788+qrutyy@users.noreply.github.com>
2024-05-23 18:27:45 +0300

    feat: add zoom control buttons

---
+++ app/src/main/kotlin/view/utils/ZoomBox.kt
@@ -0,0 +1,33 @@
+package view.utils
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ZoomBox(currentScale: MutableState<Float>) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.BottomEnd
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp),
+            horizontalAlignment = Alignment.Start,
+            verticalArrangement = Arrangement.Bottom
+        ) {
+            FloatingActionButton(onClick = { currentScale.value *= 1.1f }) {
+                Text("+")
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            FloatingActionButton(onClick = { currentScale.value /= 1.1f }) {
+                Text("-")
+            }
+        }
+    }
+}
