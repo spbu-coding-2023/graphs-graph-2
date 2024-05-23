065bd81413b5a8babfb2ac0b23c17ebe5157af64
qruty <64466788+qrutyy@users.noreply.github.com>
2024-05-23 18:28:51 +0300

    refactor: clean up the MainScreen file

--- app/src/main/kotlin/view/MainScreen.kt
+++ app/src/main/kotlin/view/MainScreen.kt
@@ -1,89 +1,34 @@
 package view
 
 import MyAppTheme
-import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.background
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.pager.HorizontalPager
-import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
-import androidx.compose.material.TabRowDefaults.tabIndicatorOffset
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.unit.dp
 import view.graph.GraphView
-import view.tabScreen.AnalyzeTab
-import view.tabScreen.FileControlTab
-import view.tabScreen.GeneralTab
-import view.tabScreen.SelectTabRow
+import view.tabScreen.TabHandler
 import view.utils.FAQBox
+import view.utils.ZoomBox
 import viewmodel.MainScreenViewModel
 
-
-@OptIn(ExperimentalFoundationApi::class, ExperimentalComposeUiApi::class)
 @Composable
 fun <D> MainScreen(viewmodel: MainScreenViewModel<D>) {
     MyAppTheme {
         // state for hover effect
         val interactionSource = remember { MutableInteractionSource() }
+        val scale = remember { mutableStateOf(1f) }
 
         Row {
-            // Column with tabs and content
-            Column(
-                modifier =
-                Modifier.width(360.dp)
-                    .background(color = MaterialTheme.colors.surface)
-                    .fillMaxHeight()
-                    .clip(shape = RoundedCornerShape(10.dp))
-            ) {
-                val pageState = rememberPagerState(initialPage = 0, pageCount = { 3 })
-                val tabs = listOf("General", "Analyze", "Save & Load")
-
-                TabRow(
-                    selectedTabIndex = pageState.currentPage,
-                    contentColor = MaterialTheme.colors.surface,
-                    backgroundColor = MaterialTheme.colors.secondary,
-                    divider = {}, // to remove divider between
-                    indicator = { tabPositions ->
-                        TabRowDefaults.Indicator(
-                            modifier =
-                            Modifier.tabIndicatorOffset(tabPositions[pageState.currentPage]),
-                            height = 0.dp
-                        )
-                    },
-                    modifier = Modifier.height(50.dp)
-                ) {
-                    tabs.forEachIndexed { index, title ->
-                        SelectTabRow(pageState, index, title)
-                    }
-                }
-
-                // Content corresponding to the selected tab
-                HorizontalPager(state = pageState, userScrollEnabled = true) {
-                    Column(
-                        modifier =
-                        Modifier.width(360.dp)
-                            .background(MaterialTheme.colors.background)
-                            .fillMaxHeight(),
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        verticalArrangement = Arrangement.Top,
-                    ) {
-                        when (pageState.currentPage) {
-                            0 -> GeneralTab(viewmodel.graphViewModel)
-                            1 -> AnalyzeTab(viewmodel.graphViewModel)
-                            2 -> FileControlTab(viewmodel.graphViewModel)
-                        }
-                    }
-                }
-            }
-            Surface(modifier = Modifier.fillMaxSize()) { GraphView(viewmodel.graphViewModel) }
+            TabHandler(viewmodel)
+            Surface(modifier = Modifier.fillMaxSize()) { GraphView(viewmodel.graphViewModel, scale) }
         }
         // Hoverable box over the existing Surface
         FAQBox(interactionSource, viewmodel)
+
+        ZoomBox(scale)
     }
 }
