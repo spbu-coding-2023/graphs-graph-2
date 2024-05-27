9dd270f54a1f7f74ec377a31f5252321a94ecec9
Vlasenco Daniel <vlasenko.daniil26@gmail.com>
2024-05-27 13:50:37 +0300

    feat: add function to save graph with name to neo4j

--- app/src/main/kotlin/model/io/neo4j/Neo4jRepository.kt
+++ app/src/main/kotlin/model/io/neo4j/Neo4jRepository.kt
@@ -1,65 +1,58 @@
 package model.io.neo4j
 
-import model.UndirectedGraph
-import model.abstractGraph.Edge
 import model.abstractGraph.Graph
 import model.abstractGraph.Vertex
 import org.neo4j.driver.AuthTokens
 import org.neo4j.driver.GraphDatabase
-import org.neo4j.driver.Values
 import java.io.Closeable
 
-class Neo4jRepository<D>(
-    uri: String,
-    user: String,
-    password: String
-) : Closeable {
+const val DIR_LABEL = "POINTS_TO"
+const val UNDIR_LABEL = "CONNECTED_TO"
 
+class Neo4jRepository<D>(uri: String, user: String, password: String) : Closeable {
     private val driver = GraphDatabase.driver(uri, AuthTokens.basic(user, password))
-
-    init {
-        driver.verifyConnectivity()
-    }
     private val session = driver.session()
 
-    private val vertexToDbIdMap = mutableMapOf<Vertex<D>, Int>()
-    private var nextDbId = 0
+    private val vertexToIdMap = mutableMapOf<Vertex<D>, Int>()
+    private var nextId = 0
 
-    fun addVertex(vertex: Vertex<D>) {
-        vertexToDbIdMap[vertex] = nextDbId
-        val id = nextDbId++
+    fun saveGraph(graph: Graph<D>, name: String, isDirected: Boolean, isWeighted: Boolean) {
+        val vertices = graph.getVertices()
+        val edges = graph.getEdges()
 
-        val data = vertex.data.toString()
+        val weightMap = if (isWeighted) graph.getWeightMap() else null
+        val edgeLabel = if (isDirected) DIR_LABEL else UNDIR_LABEL
 
-        session.executeWrite {
-            it.run(
-                "CREATE (:Vertex {id:\$id, data:\$data})",
-                Values.parameters("id", id, "data", data)
-            )
-        }
-    }
+        session.executeWrite { tx ->
+            for (vertex in vertices) {
+                vertexToIdMap[vertex] = nextId
+                val id = nextId++
 
-    fun addEdge(edge: Edge<D>) {
-        val v1 = edge.vertex1
-        val v2 = edge.vertex2
+                val data = vertex.data.toString()
 
-        val id1 = vertexToDbIdMap[v1]
-        val id2 = vertexToDbIdMap[v2]
-
-        val data1 = v1.data.toString()
-        val data2 = v2.data.toString()
-
-        session.executeWrite {
-            it.run(
-                "MATCH (v:Vertex {id:\$id1, data:\$data1}) " +
-                "MATCH (u:Vertex {id:\$id2, data:\$data2}) " +
-                "CREATE (v)-[e:Edge]->(u)",
-
-                Values.parameters(
-                    "id1", id1, "id2", id2,
-                    "data1", data1, "data2", data2
+                tx.run(
+                    "CREATE (:$name {id:$id, data:$data}) "
                 )
-            )
+            }
+
+            for (edge in edges) {
+                val v1 = edge.vertex1
+                val v2 = edge.vertex2
+
+                val id1 = vertexToIdMap[v1]
+                val id2 = vertexToIdMap[v2]
+
+                val data1 = v1.data.toString()
+                val data2 = v2.data.toString()
+
+                val weight = weightMap?.get(edge)
+
+                tx.run(
+                    "MATCH (v:$name {id:$id1, data:$data1}) " +
+                    "MATCH (u:$name {id:$id2, data:$data2}) " +
+                    "CREATE (v)-[:$edgeLabel {weight:$weight}]->(u) "
+                )
+            }
         }
     }
 
