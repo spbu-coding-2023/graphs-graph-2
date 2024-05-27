349b680bcda4e19b627b1beeff891df4084b4b4c
Mike Gavrilenko <64466788+qrutyy@users.noreply.github.com>
2024-05-27 23:04:38 +0300

    test: enhance app with integration test draft #46
    * build: add new dependencies for int. tests
    
    * test: implement first integration test
    
    * feat: add getter for edge's direction type
    
    * refactor: move graphs to a separate package in module
    
    * feat: add test-tags
    
    * fix: add tag for 'not hoverable' FAQ
    
    * refactor: edit test name

--- app/src/main/kotlin/view/utils/FAQBox.kt
+++ app/src/main/kotlin/view/utils/FAQBox.kt
@@ -15,6 +15,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -27,7 +28,8 @@ fun <D> FAQBox(interactionSource: MutableInteractionSource, viewmodel: MainScree
 
     Box(
         modifier = Modifier
-            .fillMaxSize(),
+            .fillMaxSize()
+            .testTag("FAQBox"),
         contentAlignment = Alignment.TopEnd
     ) {
 
---
+++ app/src/main/kotlin/viewmodel/graph/CreateGraphViewModel.kt
@@ -0,0 +1,102 @@
+package viewmodel.graph
+
+import androidx.compose.runtime.Composable
+import model.graphs.DirectedGraph
+import model.graphs.UndirectedGraph
+import model.graphs.WeightedDirectedGraph
+import model.graphs.WeightedUndirectedGraph
+import view.MainScreen
+import viewmodel.MainScreenViewModel
+
+class CreateGraphViewModel {
+    sealed class GraphType {
+        object Integer : GraphType()
+        object UInteger : GraphType()
+        object String : GraphType()
+    }
+
+    sealed class GraphStructure {
+        object Directed : GraphStructure()
+        object Undirected : GraphStructure()
+    }
+
+    sealed class Weight {
+        object Weighted : Weight()
+        object Unweighted : Weight()
+    }
+
+    @Composable
+    fun createGraph(
+        storedData: GraphType,
+        graphStructure: GraphStructure,
+        weight: Weight
+    ) { // TODO looks too shitty (((((((((( + mb any could be changed
+        return when (weight) {
+            is Weight.Weighted -> {
+                when (graphStructure) {
+                    is GraphStructure.Directed -> {
+                        when (storedData) {
+                            is GraphType.Integer -> MainScreen(MainScreenViewModel(
+                                WeightedDirectedGraph<Int>(),
+                                "WeightedDirectedGraph Int"))
+                            is GraphType.UInteger -> MainScreen(MainScreenViewModel(
+                                WeightedDirectedGraph<UInt>(),
+                                "WeightedDirectedGraph UInt"))
+                            is GraphType.String -> MainScreen(MainScreenViewModel(
+                                WeightedDirectedGraph<String>(),
+                                "WeightedDirectedGraph String"))
+
+                        }
+                    }
+
+                    is GraphStructure.Undirected -> {
+                        when (storedData) {
+                            is GraphType.Integer -> MainScreen(MainScreenViewModel(
+                                WeightedUndirectedGraph<Int>(),
+                                "WeightedUndirectedGraph Int"))
+                            is GraphType.UInteger -> MainScreen(MainScreenViewModel(
+                                WeightedUndirectedGraph<UInt>(),
+                                "WeightedUndirectedGraph UInt"))
+                            is GraphType.String -> MainScreen(MainScreenViewModel(
+                                WeightedUndirectedGraph<String>(),
+                                "WeightedUndirectedGraph String")
+                            )
+                        }
+                    }
+                }
+            }
+
+            is Weight.Unweighted -> {
+                when (graphStructure) {
+                    is GraphStructure.Directed -> {
+                        when (storedData) {
+                            is GraphType.Integer -> MainScreen(MainScreenViewModel(
+                                DirectedGraph<Int>(),
+                                "DirectedGraph Int"))
+                            is GraphType.UInteger -> MainScreen(MainScreenViewModel(
+                                DirectedGraph<UInt>(),
+                                "DirectedGraph UInt"))
+                            is GraphType.String -> MainScreen(MainScreenViewModel(
+                                DirectedGraph<String>(),
+                                "DirectedGraph String"))
+                        }
+                    }
+
+                    is GraphStructure.Undirected -> {
+                        when (storedData) {
+                            is GraphType.Integer -> MainScreen(MainScreenViewModel(
+                                UndirectedGraph<Int>(),
+                                "UndirectedGraph Int"))
+                            is GraphType.UInteger -> MainScreen(MainScreenViewModel(
+                                UndirectedGraph<UInt>(),
+                                "UndirectedGraph UInt"))
+                            is GraphType.String -> MainScreen(MainScreenViewModel(
+                                UndirectedGraph<String>(),
+                                "UndirectedGraph String"))
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
