ca076c344a32b651805a80fd495a0e00c79f9b0a
qruty <64466788+qrutyy@users.noreply.github.com>
2024-05-27 17:18:19 +0300

    feat: implement DB handling. TOFIX: import

--- app/src/main/kotlin/model/io/sql/SQLDatabaseModule.kt
+++ app/src/main/kotlin/model/io/sql/SQLDatabaseModule.kt
@@ -1,13 +1,31 @@
-import model.graphs.abstractGraph.Edge
-import viewmodel.graph.GraphViewModel
-import java.sql.*
+package model.io.sql
 
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.runtime.*
+import kotlinx.coroutines.*
+import model.graphs.abstractGraph.Graph
+import view.MainScreen
+import view.utils.ErrorWindow
+import view.utils.getGraphVMParameter
+import viewmodel.MainScreenViewModel
+import viewmodel.graph.GraphViewModel
+import viewmodel.graph.SetupGraphViewModel
 import java.io.File
+import java.sql.*
 
 object SQLDatabaseModule {
     private const val DB_DIRECTORY = "database"
     private const val DB_NAME = "my_graph_database.db"
+    private const val QUERY_NAME = "queries.txt"
+    private const val QUERY_DIRECTORY = "src/main/kotlin/model/io/sql/"
     private val DB_URL = "jdbc:sqlite:${File(DB_DIRECTORY, DB_NAME).absolutePath}"
+
+    private fun readQueriesFromFile(): String {
+        val fileContent = File(QUERY_DIRECTORY, QUERY_NAME).readText()
+        return fileContent.trim()  // Trim any leading/trailing whitespace
+    }
+
+    private val insertQueries = readQueriesFromFile()
 
     init {
         createDatabaseDirectory()
@@ -25,33 +43,8 @@ object SQLDatabaseModule {
         return DriverManager.getConnection(DB_URL)
     }
 
-    fun createTables() {
-        val createTableSQL = """
-        PRAGMA foreign_keys = ON;
-
-        CREATE TABLE IF NOT EXISTS graphs (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT NOT NULL,
-            graph_type TEXT NOT NULL,
-            stored_value_type TEXT NOT NULL
-        );
-
-        CREATE TABLE IF NOT EXISTS edges (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            graph_id INTEGER NOT NULL,
-            vertex1_id INTEGER NOT NULL,
-            vertex2_id INTEGER NOT NULL,
-            weight INTEGER,
-            FOREIGN KEY (graph_id) REFERENCES graphs(id)
-        );
-        
-        CREATE TABLE IF NOT EXISTS vertices (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            graph_id INTEGER NOT NULL,
-            data TEXT,
-            FOREIGN KEY (graph_id) REFERENCES graphs(id)
-        );
-    """.trimIndent()
+    private fun createTables() {
+        val createTableSQL = insertQueries.split(":")[7].trimIndent()
 
         val connection = getConnection()
         connection.use { conn ->
@@ -63,9 +56,9 @@ object SQLDatabaseModule {
 
 
     fun <D> insertGraph(graphVM: GraphViewModel<D>, graphName: String, graphType: String) {
-        val insertGraphSQL = "INSERT INTO graphs(name, graph_type, stored_value_type) VALUES (?, ?, ?)"
-        val insertEdgesSQL = "INSERT INTO edges(graph_id, vertex1_id, vertex2_id, weight) VALUES (?, ?, ?, ?)"
-        val insertVerticesSQL = "INSERT INTO vertices(graph_id, data) VALUES (?, ?)"
+        val insertGraphSQL = insertQueries.split(":")[0]
+        val insertEdgesSQL = insertQueries.split(":")[1]
+        val insertVerticesSQL = insertQueries.split(":")[2]
 
         getConnection().use { connection ->
             connection.autoCommit = false
@@ -99,7 +92,6 @@ object SQLDatabaseModule {
                 graphVM.graph.getVertices().forEach { vertex ->
                     connection.prepareStatement(insertVerticesSQL).use { statement ->
                         statement.setInt(1, graphId)
-                        println(vertex.data)
                         statement.setString(2, vertex.data.toString())
                         statement.executeUpdate()
                     }
@@ -115,36 +107,185 @@ object SQLDatabaseModule {
         }
     }
 
+    fun getGraphNames(): ArrayList<Pair<Int, String>> {
+        val selectNamesSQL = insertQueries.split(":")[3]
+        val graphNames = arrayListOf<Pair<Int, String>>()
+
+        try {
+            getConnection().use { connection ->
+                connection.prepareStatement(selectNamesSQL).use { statement ->
+                    val resultSet = statement.executeQuery()
+                    while (resultSet.next()) {
+                        graphNames.add(Pair(resultSet.getInt("id"), resultSet.getString("name")))
+                    }
+                }
+            }
+        } catch (e: SQLException) {
+            // ErrorWindow(e.message.toString(), { System.exit(0) })
+        }
+
+        return graphNames
+    }
 
 
-//    fun insertUser(name: String, age: Int) {
-//        val insertSQL = "INSERT INTO users(name, age) VALUES (?, ?)"
-//        getConnection().use { connection ->
-//            connection.prepareStatement(insertSQL).use { statement ->
-//                statement.setString(1, name)
-//                statement.setInt(2, age)
-//                statement.executeUpdate()
-//            }
-//        }
-//    }
+    @Composable
+    fun <D> importGraph(graphId: Int) {
+        val graphVMState = remember { mutableStateOf<GraphViewModel<D>?>(null) }
+        val showErrorMessage = remember { mutableStateOf(false) }
 
-//    fun <D> getEdges(): List<Edge<D>> {
-//        val selectSQL = "SELECT * FROM edges"
-//        val users = mutableListOf<Edge<D>>()
-//        getConnection().use { connection ->
-//            connection.createStatement().use { statement ->
-//                val resultSet = statement.executeQuery(selectSQL)
-//                while (resultSet.next()) {
-//                    users.add(
-//                        Edge<D>(
-//                            id = resultSet.getInt("id"),
-//                            name = resultSet.getString("name"),
-//                            age = resultSet.getInt("age")
-//                        )
-//                    )
-//                }
-//            }
-//        }
-//        return users
-//    }
+        LaunchedEffect(graphId) {
+            try {
+                val currentGraphSetup = withContext(Dispatchers.Main) {
+                    val connection = getConnection()
+                    connection.use {
+                        val selectGraphSQL = insertQueries.split(":")[6]
+                        it.prepareStatement(selectGraphSQL).use { statement ->
+                            statement.setInt(1, graphId)
+                            val resultSet = statement.executeQuery()
+
+                            if (resultSet.next()) {
+                                importGraphInfo(graphId)
+                            } else {
+                                showErrorMessage.value = true
+                                throw SQLException("Graph with ID $graphId not found.")
+                            }
+                        }
+                    }
+                }
+
+                // Execute side-effect to create graph object
+                SetupGraphViewModel().createGraphObject(
+                    currentGraphSetup.first,
+                    currentGraphSetup.second,
+                    currentGraphSetup.third,
+                    graphId,
+                    graphVMState
+                )
+
+            } catch (e: SQLException) {
+                e.printStackTrace()
+                showErrorMessage.value = true
+            }
+        }
+
+        // This part of the code (after fix) will be recomposed whenever graphVMState changes
+            importGraphUI(showErrorMessage, graphVMState, graphId)
+
+    }
+
+
+    @Composable
+    fun <D> importGraphUI(
+        showErrorMessage: MutableState<Boolean>,
+        graphVMState: MutableState<GraphViewModel<D>?>,
+        graphId: Int
+    ) {
+        if (showErrorMessage.value) ErrorWindow("Graph with ID $graphId not found.", { System.exit(-1) })
+
+        println(graphVMState.value == null)
+        if (graphVMState.value != null) {
+            graphVMState.value?.graph?.getVertices()?.forEach { println(it.data) }
+            graphVMState.value?.graph?.getEdges()?.forEach { println(it.vertex1) }
+            MainScreen(MainScreenViewModel(graphVMState.value!!.graph, graphVMState.value!!.graphType.value, graphVMState.value))
+        }
+        else CircularProgressIndicator()
+    }
+
+    fun <D> updateImportedGraphVM(
+        graph: Graph<D>,
+        graphId: Int,
+        graphVMState: MutableState<GraphViewModel<D>?>
+    ): GraphViewModel<D>? {
+        val selectGraphSQL = insertQueries.split(":")[6]
+        val selectVerticesSQL = insertQueries.split(":")[4]
+        val selectEdgesSQL = insertQueries.split(":")[5]
+
+        CoroutineScope(Dispatchers.Main).launch {
+            try {
+                getConnection().use { connection ->
+                    connection.prepareStatement(selectGraphSQL).use { statement ->
+                        statement.setInt(1, graphId)
+                        val resultSet = statement.executeQuery()
+                        if (resultSet.next()) {
+                            val currentGraphSetup = importGraphInfo(graphId)
+                            val graphVMType =
+                                mutableStateOf(currentGraphSetup.second.toString() + "Graph" + " " + currentGraphSetup.first.toString())
+
+                            val graphVM = GraphViewModel(
+                                graph,
+                                mutableStateOf(false),
+                                mutableStateOf(false),
+                                graphVMType,
+                                mutableStateOf(currentGraphSetup.second.toString().contains("Directed"))
+                            )
+
+                            // Fetch vertices
+                            connection.prepareStatement(selectVerticesSQL).use { vertexStatement ->
+                                vertexStatement.setInt(1, graphId)
+                                val vertexResultSet = vertexStatement.executeQuery()
+                                while (vertexResultSet.next()) {
+                                    val vertexData = vertexResultSet.getString("data")
+                                    graphVM.addVertex(vertexData)
+                                }
+                            }
+
+                            // Fetch edges
+                            connection.prepareStatement(selectEdgesSQL).use { edgeStatement ->
+                                edgeStatement.setInt(1, graphId)
+                                val edgeResultSet = edgeStatement.executeQuery()
+                                while (edgeResultSet.next()) {
+                                    val vertex1Id = edgeResultSet.getInt("vertex1_id")
+                                    val vertex2Id = edgeResultSet.getInt("vertex2_id")
+                                    val weight = edgeResultSet.getInt("weight")
+
+                                    graphVM.addEdge(vertex1Id, vertex2Id)
+                                    // TODO: add weight
+                                }
+                            }
+                            withContext(Dispatchers.Main) {
+                                graphVMState.value = graphVM
+                            }
+                        } else {
+                            throw SQLException("Graph with ID $graphId not found.")
+                        }
+                    }
+                }
+            } catch (e: SQLException) {
+                e.printStackTrace()
+            }
+        }
+        return graphVMState.value
+    }
+
+
+
+
+    private fun importGraphInfo(graphId: Int): Triple<SetupGraphViewModel.GraphType, SetupGraphViewModel.GraphStructure, SetupGraphViewModel.Weight> {
+        val selectGraphSQL = insertQueries.split(":")[6]
+        var graphStructure: Int?
+        var graphWeight: Int?
+        var storedValueType: Int?
+        getConnection().use { connection ->
+            connection.prepareStatement(selectGraphSQL).use { statement ->
+                statement.setInt(1, graphId)
+                val resultSet = statement.executeQuery()
+                // val graphName = resultSet.getString("name") // TODO
+                val graphType = resultSet.getString("graph_type")
+
+                graphStructure = if (graphType.contains("Undirected")) 0 else 1
+                graphWeight = if (graphType.contains("Weighted")) 0 else 1
+
+                storedValueType =
+                    if (resultSet.getString("stored_value_type") == "Int") 0
+                    else if (resultSet.getString("stored_value_type") == "UInt") 1 else 2
+            }
+        }
+        return graphWeight?.let {
+            storedValueType?.let { it1 ->
+                graphStructure?.let { it2 ->
+                    getGraphVMParameter(it1, it2, it)
+                }
+            }
+        } ?: throw NoSuchElementException("")
+    }
 }
