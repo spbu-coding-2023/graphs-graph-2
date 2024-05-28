e2b4f577c346de30d1bd2d90bc152f7335333693
Karim Shakirov <karimshak06@gmail.com>
2024-05-28 14:16:24 +0300

    feat: add clustering algorithm #47
    * feat: add louvain clustering algorithm implementation by jetbrains-research
    
    * feat: add class for running Louvain algorithm
    
    * test: add tests for clustering
    
    * chore: add license for used source code

---
+++ app/src/main/kotlin/model/algorithms/clustering/LouvainClustering.kt
@@ -0,0 +1,68 @@
+package model.algorithms.clustering
+
+import model.graphs.WeightedDirectedGraph
+import model.graphs.WeightedUndirectedGraph
+import model.graphs.abstractGraph.Graph
+import model.graphs.abstractGraph.Vertex
+import model.algorithms.clustering.implementation.Link
+import model.algorithms.clustering.implementation.getPartition
+
+class LouvainClustering {
+    fun <D> findClusters(graph: Graph<D>): Set<Set<Vertex<D>>> {
+        if (graph.getVertices().size == 1) return setOf(setOf(graph.getVertices()[0]))
+
+        val links = convertToAPIFormat(graph)
+
+        val result = getPartition(links)
+
+        return convertResult(result, graph)
+    }
+
+    private fun <D> convertToAPIFormat(graph: Graph<D>): List<Link> {
+        val links = mutableListOf<Link>()
+
+        for (edge in graph.getEdges()) {
+            val id1 = edge.vertex1.id
+            val id2 = edge.vertex2.id
+
+            val weight: Double =
+                when (graph) {
+                    is WeightedUndirectedGraph -> graph.getWeight(edge).toDouble()
+                    is WeightedDirectedGraph -> graph.getWeight(edge).toDouble()
+                    else -> 1.0
+                }
+
+            links += EdgeLink(id1, id2, weight)
+        }
+
+        return links
+    }
+
+    private fun <D> convertResult(resultMap: Map<Int, Int>, graph: Graph<D>): Set<Set<Vertex<D>>> {
+        val vertices = graph.getVertices()
+        val communities = mutableSetOf<Set<Vertex<D>>>()
+
+        var currentCommunityId = 0
+        var currentCommunity = mutableSetOf<Vertex<D>>()
+
+        for (vertexId in resultMap.keys) {
+            if (resultMap[vertexId] == currentCommunityId) currentCommunity += vertices[vertexId]
+            else {
+                communities += currentCommunity
+
+                currentCommunityId++
+                currentCommunity = mutableSetOf()
+
+                currentCommunity += vertices[vertexId]
+            }
+        }
+
+        return communities
+    }
+
+    inner class EdgeLink(private val vertex1Id: Int, private val vertex2Id: Int, val weight: Double) : Link {
+        override fun source() = vertex1Id
+        override fun target() = vertex2Id
+        override fun weight() = weight
+    }
+}
---
+++ app/src/main/kotlin/model/algorithms/clustering/implementation/Node.kt
@@ -0,0 +1,55 @@
+/**
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package model.algorithms.clustering.implementation
+
+/**
+ * Simple link representation used inside Node and Louvain algorithm.
+ */
+internal class InternalLink(
+    val to: Int,
+    val weight: Double
+)
+
+/**
+ * @param community Index of a community to which the node has been assigned.
+ * @param selfLoopsWeight Weight of all links that start and end at originalNodes making up this node * 2.
+ */
+internal sealed class BaseNode(
+    var community: Int,
+    val originalNodes: Set<Int>,
+    open val incidentLinks: List<InternalLink>,
+    open val selfLoopsWeight: Double
+) {
+    fun neighbourCommunities(nodes: List<Node>) = incidentLinks.map { nodes[it.to].community }.distinct().filter { it != community }
+}
+
+internal class Node(
+    community: Int,
+    originalNodes: Set<Int>,
+    incidentLinks: List<InternalLink>,
+    selfLoopsWeight: Double = 0.0
+) : BaseNode(community, originalNodes, incidentLinks, selfLoopsWeight) {
+    val outDegree = incidentLinks.sumOf { it.weight }
+    fun degree() = outDegree + selfLoopsWeight
+}
+
+internal class MutableNode(
+    community: Int,
+    originalNodes: Set<Int>,
+    override val incidentLinks: MutableList<InternalLink> = mutableListOf(),
+    override var selfLoopsWeight: Double = 0.0
+) : BaseNode(community, originalNodes, incidentLinks, selfLoopsWeight) {
+    fun toNode(): Node = Node(community, originalNodes, incidentLinks)
+}
